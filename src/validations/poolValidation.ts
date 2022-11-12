9994429fc6bc48413bac4daf1434cadff99e3ffc
Gui-dev <adrianon10@gmail.com>
2022-11-12 04:19:56 -0300

    feat: add validations poolJoinBody and getPoolParams

--- src/validations/poolValidation.ts
+++ src/validations/poolValidation.ts
@@ -3,3 +3,11 @@ import { z } from 'zod'
 export const createPoolBody = z.object({
   title: z.string()
 })
+
+export const poolJoinBody = z.object({
+  code: z.string()
+})
+
+export const getPoolParams = z.object({
+  id: z.string()
+})
