66f88eaa3e0df1bdffc79b09a9123fb6e312bfea
Gui-dev <adrianon10@gmail.com>
2022-11-14 03:56:55 -0300

    feat: add new route

--- src/routes/game.routes.ts
+++ src/routes/game.routes.ts
@@ -1,6 +1,10 @@
-import { FastifyRequest, FastifyReply, FastifyInstance } from 'fastify'
-import { prisma } from '../services/prisma'
+import { FastifyInstance } from 'fastify'
+
+import { GameController } from './../controllers/GameController'
+import { authenticate } from '../plugins/authenticate'
+
+const gameController = new GameController()
 
 export const gameRoutes = async (fastify: FastifyInstance) => {
-
+  fastify.get('/pools/:id/games', { onRequest: [authenticate] }, gameController.index)
 }
